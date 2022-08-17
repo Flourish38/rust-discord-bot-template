49f56a220e2f0e81d11d2a5f6824e6281fd17030
brsan <brsandvik@gmail.com>
2022-08-17 14:45:53 -0700

    Refactor commands into separate functions also added permission checking for shutdown

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,29 @@
+// based on https://github.com/serenity-rs/serenity/blob/current/examples/e14_slash_commands/src/main.rs
+
 use std::fs;
+use std::time::Instant;
 
 use serenity::async_trait;
-use serenity::model::application::command::{Command, CommandOptionType};
-use serenity::model::application::interaction::application_command::CommandDataOptionValue;
+use serenity::model::application::command::Command;
 use serenity::model::application::interaction::{Interaction, InteractionResponseType};
 use serenity::model::gateway::Ready;
+use serenity::model::channel::Message;
 use serenity::model::application::interaction::application_command::ApplicationCommandInteraction;
+use serenity::model::id::UserId;
 use serenity::prelude::*;
 
 // needed for shutdown command
 use lazy_static::lazy_static;
-use std::sync::mpsc::{Sender, channel};
+use tokio::sync::mpsc::{Sender, channel};
 
 lazy_static! { static ref SHUTDOWN_SENDER: Mutex<Option<Sender<bool>>> = Mutex::new(None); }
+
+lazy_static! { static ref ADMIN_USERS: Vec<UserId> = vec![UserId(165216105197993984)]; }
 
 struct Handler;
 
 // for some reason if you don't specify the return type the compiler doesn't figure it out
-async fn send_interaction_response_message(ctx: &Context, command: &ApplicationCommandInteraction, content: &String) -> Result<(), SerenityError> {
+async fn send_interaction_response_message<D>(ctx: &Context, command: &ApplicationCommandInteraction, content: D) -> Result<(), SerenityError> where D: ToString {
     command.create_interaction_response(&ctx.http, |response| {
             response
                 .kind(InteractionResponseType::ChannelMessageWithSource)
@@ -26,71 +32,59 @@ async fn send_interaction_response_message(ctx: &Context, command: &ApplicationC
         .await
 }
 
+async fn edit_interaction_response_message<D>(ctx: &Context, command: &ApplicationCommandInteraction, content: D) -> Result<Message, SerenityError> where D: ToString {
+    command.edit_original_interaction_response(&ctx.http, |response| {
+        response.content(content)
+    })
+    .await
+}
+
+async fn nyi_command(ctx: Context, command: ApplicationCommandInteraction) -> Result<(), SerenityError> {
+    send_interaction_response_message(&ctx, &command, "This command hasn't been implemented. Try /help").await
+}
+
+async fn ping_command(ctx: Context, command: ApplicationCommandInteraction) -> Result<(), SerenityError> {
+    let start_time = Instant::now();
+    send_interaction_response_message(&ctx, &command, "Pong!").await?;
+    let mut duration = start_time.elapsed().as_millis().to_string();
+    duration.push_str(" ms");
+    edit_interaction_response_message(&ctx, &command, duration).await?;
+    Ok(())
+}
+
+async fn shutdown_command(ctx: Context, command: ApplicationCommandInteraction) -> Result<(), SerenityError> {
+    if !ADMIN_USERS.contains(&command.user.id) {
+        send_interaction_response_message(&ctx, &command, "You do not have permission.").await?;
+        return Ok(())
+    }
+    send_interaction_response_message(&ctx, &command, "Shutting down...").await?;
+    let lock = SHUTDOWN_SENDER.lock().await;
+    let sender = &lock.as_ref().expect("Shutdown command called before shutdown channel initialized??");
+    sender.send(true).await.expect("Shutdown message send error");
+    println!("Passing shutdown message");
+    drop(lock);
+    ctx.shard.shutdown_clean();
+    Ok(())
+}
+
 #[async_trait]
 impl EventHandler for Handler {
     async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
-        if let Interaction::ApplicationCommand(command) = interaction {
-            // println!("Received command interaction: {:#?}", command);
-
-            let content = match command.data.name.as_str() {
-                "ping" => "Hey, I'm alive!".to_string(),
-                "shutdown" => {
-                    let lock = SHUTDOWN_SENDER.lock().await;
-                    let sender = &lock.as_ref().expect("Shutdown command called before shutdown channel initialized??");
-                    sender.send(true).expect("Shutdown message send error");
-                    drop(lock);
-                    println!("Passing shutdown message");
-                    "Shutting down...".to_string()
-                },
-                "id" => {
-                    let options = command
-                        .data
-                        .options
-                        .get(0)
-                        .expect("Expected user option")
-                        .resolved
-                        .as_ref()
-                        .expect("Expected user object");
-
-                    if let CommandDataOptionValue::User(user, _member) = options {
-                        format!("{}'s id is {}", user.tag(), user.id)
-                    } else {
-                        "Please provide a valid user".to_string()
-                    }
-                },
-                "attachmentinput" => {
-                    let options = command
-                        .data
-                        .options
-                        .get(0)
-                        .expect("Expected attachment option")
-                        .resolved
-                        .as_ref()
-                        .expect("Expected attachment object");
-
-                    if let CommandDataOptionValue::Attachment(attachment) = options {
-                        format!(
-                            "Attachment name: {}, attachment size: {}",
-                            attachment.filename, attachment.size
-                        )
-                    } else {
-                        "Please provide a valid attachment".to_string()
-                    }
-                },
-                _ => "not implemented :(".to_string(),
-            };
-
-            if let Err(why) = send_interaction_response_message(&ctx, &command, &content).await
-            {
+        if let Interaction::ApplicationCommand(command) = interaction {            
+            if let Err(why) = match command.data.name.as_str() {
+                "ping" => ping_command(ctx, command).await,
+                "shutdown" => shutdown_command(ctx, command).await,
+                _ => nyi_command(ctx, command).await
+            } {
                 println!("Cannot respond to slash command: {}", why);
-            }
+            };
         }
     }
 
     async fn ready(&self, ctx: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
 
-        let _commands = Command::set_global_application_commands(&ctx.http, |commands| {
+        Command::set_global_application_commands(&ctx.http, |commands| {
             commands
                 .create_application_command(|command| {
                     command.name("ping").description("A ping command")
@@ -98,106 +92,8 @@ impl EventHandler for Handler {
                 .create_application_command(|command| {
                     command.name("shutdown").description("Shut down the bot")
                 })
-                .create_application_command(|command| {
-                    command.name("id").description("Get a user id").create_option(|option| {
-                        option
-                            .name("id")
-                            .description("The user to lookup")
-                            .kind(CommandOptionType::User)
-                            .required(true)
-                    })
-                })
-                .create_application_command(|command| {
-                    command
-                        .name("welcome")
-                        .name_localized("de", "begrüßen")
-                        .description("Welcome a user")
-                        .description_localized("de", "Einen Nutzer begrüßen")
-                        .create_option(|option| {
-                            option
-                                .name("user")
-                                .name_localized("de", "nutzer")
-                                .description("The user to welcome")
-                                .description_localized("de", "Der zu begrüßende Nutzer")
-                                .kind(CommandOptionType::User)
-                                .required(true)
-                        })
-                        .create_option(|option| {
-                            option
-                                .name("message")
-                                .name_localized("de", "nachricht")
-                                .description("The message to send")
-                                .description_localized("de", "Die versendete Nachricht")
-                                .kind(CommandOptionType::String)
-                                .required(true)
-                                .add_string_choice_localized(
-                                    "Welcome to our cool server! Ask me if you need help",
-                                    "pizza",
-                                    [("de", "Willkommen auf unserem coolen Server! Frag mich, falls du Hilfe brauchst")]
-                                )
-                                .add_string_choice_localized(
-                                    "Hey, do you want a coffee?",
-                                    "coffee",
-                                    [("de", "Hey, willst du einen Kaffee?")],
-                                )
-                                .add_string_choice_localized(
-                                    "Welcome to the club, you're now a good person. Well, I hope.",
-                                    "club",
-                                    [("de", "Willkommen im Club, du bist jetzt ein guter Mensch. Naja, hoffentlich.")],
-                                )
-                                .add_string_choice_localized(
-                                    "I hope that you brought a controller to play together!",
-                                    "game",
-                                    [("de", "Ich hoffe du hast einen Controller zum Spielen mitgebracht!")],
-                                )
-                        })
-                })
-                .create_application_command(|command| {
-                    command
-                        .name("numberinput")
-                        .description("Test command for number input")
-                        .create_option(|option| {
-                            option
-                                .name("int")
-                                .description("An integer from 5 to 10")
-                                .kind(CommandOptionType::Integer)
-                                .min_int_value(5)
-                                .max_int_value(10)
-                                .required(true)
-                        })
-                        .create_option(|option| {
-                            option
-                                .name("number")
-                                .description("A float from -3.3 to 234.5")
-                                .kind(CommandOptionType::Number)
-                                .min_number_value(-3.3)
-                                .max_number_value(234.5)
-                                .required(true)
-                        })
-                })
-                .create_application_command(|command| {
-                    command
-                        .name("attachmentinput")
-                        .description("Test command for attachment input")
-                        .create_option(|option| {
-                            option
-                                .name("attachment")
-                                .description("A file")
-                                .kind(CommandOptionType::Attachment)
-                                .required(true)
-                        })
-                })
         })
-        .await;
-
-        // println!("I now have the following guild slash commands: {:#?}", commands);
-
-        let _guild_command = Command::create_global_application_command(&ctx.http, |command| {
-            command.name("wonderful_command").description("An amazing command")
-        })
-        .await;
-
-        // println!("I created the following global slash command: {:#?}", guild_command);
+        .await.expect("Failed to set application commands");
     }
 }
 
@@ -212,14 +108,14 @@ async fn main() {
         .await
         .expect("Error creating client");
 
-    let (sender, receiver) = channel();
+    let (sender, mut receiver) = channel(64);
     *SHUTDOWN_SENDER.lock().await = Some(sender);
 
     let shard_manager = client.shard_manager.clone();
 
     tokio::spawn(async move {
         loop {
-            let b = receiver.recv().expect("Shutdown message pass error");
+            let b = receiver.recv().await.expect("Shutdown message pass error");
             if b {
                 shard_manager.lock().await.shutdown_all().await;
                 println!("Shutdown shard manager");
